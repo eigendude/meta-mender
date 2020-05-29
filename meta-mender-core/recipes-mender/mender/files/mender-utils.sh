7d17f6bcd6ea3aef8e1cb50e3d2eec094014b059
Garrett Brown <garrett.brown@aclima.io>
2020-05-29 01:19:21 -0700

    Enable installation to eMMC and dual booting
    To add a live installer to the image, this requires two steps:
    
        * Append "mender-install" to MENDER_FEATURES_ENABLE (done by default)
        * Set MENDER_INSTALL_DEVICE to "/dev/mmcblk1"
    
    U-Boot will try to boot from the SD card if present, and from eMMC if
    there is no SD card.
    
    Two device paths in the image are hard-coded to /dev/mmcklb0. On first
    boot, they are updated to /dev/mmcblk1. The files are:
    
        * /etc/fstab - Updated by mender-update-fstab.service
        * /data/u-boot/fw_env.config - Updated by mender-update-uboot.service

---
+++ meta-mender-core/recipes-mender/mender/files/mender-utils.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Reads the "root" parameter passed in the kernel command line
+function read_root_device() {
+    [ -z "${CMDLINE+x}" ] && CMDLINE=`cat /proc/cmdline`
+    for arg in ${CMDLINE}; do
+        # Set optarg to option parameter, and '' if no parameter was given
+        optarg=`expr "x$arg" : 'x[^=]*=\(.*\)' || echo ''`
+        case $arg in
+            root=*)
+                echo $optarg
+                ;;
+        esac
+    done
+}
