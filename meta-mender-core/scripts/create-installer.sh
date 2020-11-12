e9944f7e81acab4ec7587eaec67ea330ad168be0
Marquise Stein <garrett.brown@aclima.io>
2020-11-12 07:51:58 -0800

    Fix eMMC installation when 1.10 is on eMMC
    The root cause is that the bootloader will attempt to load uEnv.txt
    from SD card, fail, and then attempt to load uEnv.txt from eMMC.
    
    Therefore, we boot with the new U-Boot, Grub and kernel, but with
    old kernel command-line parameters.
    
    As such, the 'root' parameters will be a partition UUID location,
    e.g.:
    
        root=PARTUUID=976ff3fd-e86a-4a42-942b-eb7527ae9590
    
    In this case, fall back to grep'ing "mount".

--- meta-mender-core/scripts/create-installer.sh
+++ meta-mender-core/scripts/create-installer.sh
@@ -136,16 +136,30 @@ function update_progress() {
 
 # Read the "root" parameter passed in the kernel command line
 function read_root_device() {
+    root_device=
+
     cmdline=\$(cat "/proc/cmdline")
     for arg in \${cmdline}; do
         # Set optarg to option parameter, and '' if no parameter was given
         optarg=\$(expr "x\$arg" : 'x[^=]*=\(.*\)' || echo '')
         case \$arg in
             root=*)
-                echo \$optarg
+                root_device=\$optarg
                 ;;
         esac
     done
+
+    #
+    # Fall back to grepping mount if kernel is booted with a PARTUUID partition
+    # identifier, e.g.:
+    #
+    #   root=PARTUUID=976ff3fd-e86a-4a42-942b-eb7527ae9590
+    #
+    if [[ \$root_device =~ "PARTUUID" ]]; then
+        root_device=\$(mount | grep ' / ' | cut -d' ' -f 1)
+    fi
+
+    echo \$root_device
 }
 
 ################################################################################
