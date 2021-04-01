d71f9c9ebe664983cfeb1a5cd19a113391b7c3a8
Garrett Brown <garrett.brown@aclima.io>
2021-04-01 16:12:31 -0700

    Revert "Fix eMMC installation when 1.10 is on eMMC"
    This reverts commit e9944f7e81acab4ec7587eaec67ea330ad168be0.

--- meta-mender-core/recipes-mender/mender-client/files/mender-utils.sh
+++ meta-mender-core/recipes-mender/mender-client/files/mender-utils.sh
@@ -8,28 +8,14 @@
 
 # Reads the "root" parameter passed in the kernel command line
 function read_root_device() {
-    root_device=
-
     [ -z "${CMDLINE+x}" ] && CMDLINE=`cat /proc/cmdline`
     for arg in ${CMDLINE}; do
         # Set optarg to option parameter, and '' if no parameter was given
         optarg=`expr "x$arg" : 'x[^=]*=\(.*\)' || echo ''`
         case $arg in
             root=*)
-                root_device=$optarg
+                echo $optarg
                 ;;
         esac
     done
-
-    #
-    # Fall back to grepping mount if kernel is booted with a PARTUUID partition
-    # identifier, e.g.:
-    #
-    #   root=PARTUUID=976ff3fd-e86a-4a42-942b-eb7527ae9590
-    #
-    if [[ $root_device =~ "PARTUUID" ]]; then
-        root_device=$(mount | grep ' / ' | cut -d' ' -f 1)
-    fi
-
-    echo $root_device
 }
